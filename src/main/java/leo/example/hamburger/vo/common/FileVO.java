b66203103f31c02283650395f83f9d5ccc13aa16
LeoRyu-Hub <leo.ryu0113@gmail.com>
2025-01-21 17:41:07 +0900

    파일 업로드

---
+++ src/main/java/leo/example/hamburger/vo/common/FileVO.java
@@ -0,0 +1,19 @@
+package leo.example.hamburger.vo.common;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data
+public class FileVO extends FileMstVO {
+
+	@JsonIgnore
+	private MultipartFile file;
+	
+	private int width=600;
+	private int height=600;
+	private String position = "H";  // W:넓이중심, H:높이중심, X:설정한 수치로(비율무시)
+	private String format="jpg";	//파일 유형
+}
